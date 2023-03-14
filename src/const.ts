36d2c79a441aa0dc708364a680f63892cfe20ec7
opv1 <pas7273@gmail.com>
2023-03-14 21:27:46 +0300

    commit

--- src/const.ts
+++ src/const.ts
@@ -1,8 +1,8 @@
 export const fontsForLoad = [
+  '500 50px Halvar Breit',
+  '400 28px Halvar Breit',
   '400 18px PT Root UI',
   '400 14px PT Root UI',
-  '400 50px Halvar Breit',
-  '400 28px Halvar Breit',
 ]
 
 export const regexpEmail = /^[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}$/

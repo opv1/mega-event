058f46837bccee47facd5181805de1904ec013a5
opv1 <pas7273@gmail.com>
2024-01-03 11:52:11 +0300

    commit

--- cypress/component/ComponentName.cy.tsx
+++ cypress/component/ComponentName.cy.tsx
@@ -1,5 +1,7 @@
-describe('ComponentName.cy.tsx', () => {
-  it('playground', () => {
-    // cy.mount()
+// import { Input } from 'components/UI/Input'
+
+describe('Input', () => {
+  it('Input text', () => {
+    // cy.mount(<Input validationRules='required|email' />)
   })
 })
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,10 +1,12 @@
 /// <reference types="cypress" />
 
 Cypress.Commands.add('login', (email, password) => {
-  cy.get('input[name="email"]').type(email)
-  cy.get('input[name="password"]').type(password)
+  cy.get('form').within(() => {
+    cy.get('input[name="email"]').type(email)
+    cy.get('input[name="password"]').type(password)
 
-  cy.get('button').contains('Войти').click()
+    cy.get('button').contains('Войти').click()
+  })
 
   cy.url().should('include', '/questionnaire')
 })
@@ -14,32 +16,34 @@ Cypress.Commands.add(
   (type, name, position, phone, date, options) => {
     cy.get('button').contains(type).click()
 
-    cy.get('input[name="name"]').type(name)
-    cy.get('input[name="position"]').type(position)
-    cy.get('input[name="phone"]').type(phone)
+    cy.get('form').within(() => {
+      cy.get('input[name="name"]').type(name)
+      cy.get('input[name="position"]').type(position)
+      cy.get('input[name="phone"]').type(phone)
 
-    cy.get('span[data-test="value"]').click()
-    cy.get('ul li')
-      .should('have.length', 4)
-      .each(($elem) => {
-        if ($elem.text() === date) {
-          cy.wrap($elem).click()
-          return
-        }
-      })
-    cy.get('span[data-test="value"]').should('have.text', date)
+      cy.get('span[data-test="value"]').click()
+      cy.get('ul li')
+        .should('have.length', 4)
+        .each(($elem) => {
+          if ($elem.text() === date) {
+            cy.wrap($elem).click()
+            return
+          }
+        })
+      cy.get('span[data-test="value"]').should('have.text', date)
 
-    cy.get('div label')
-      .should('have.length', 3)
-      .each(($elem) => {
-        const option = $elem.attr('for')
+      cy.get('div label')
+        .should('have.length', 3)
+        .each(($elem) => {
+          const option = $elem.attr('for')
 
-        if (option && options.includes(option)) {
-          cy.wrap($elem).click()
-        }
-      })
+          if (option && options.includes(option)) {
+            cy.wrap($elem).click()
+          }
+        })
 
-    cy.get('button').contains('Отправить заявку').click()
+      cy.get('button').contains('Отправить заявку').click()
+    })
 
     cy.url().should('include', '/success')
   },
